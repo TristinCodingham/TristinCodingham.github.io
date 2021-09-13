bc643e8e2bc277d2311a00c0795a0e39e3d0b2c7
TristinCodingham <tctristinc72@gmail.com>
2021-09-13 14:12:38 +0200

    Edit nav

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
 const navUl = document.getElementById("nav-ul");
 const button = document.getElementById("nav-hamburger");
+let isOpen = false;
 
 const toggleNav = () => {
     navUl.classList.toggle("toggle-nav");
+    isOpen = true;
 }
 
-button.onclick = toggleNav;
-button.onactivate = toggleNav;
+const closeNav = () => {
+    if(isOpen) {
+        navUl.classList.remove("toggle-nav");
+    }
+}
+
+//window.addEventListener('click', closeNav);
+button.addEventListener('click', toggleNav);
