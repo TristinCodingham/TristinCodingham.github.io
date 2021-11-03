bd8a934f34a7faccbd1885e3c9a3bf8efda2897b
TristinCodingham <tctristinc72@gmail.com>
2021-11-03 16:12:19 +0200

    Update: Navbar, Main, Logo

--- scripts.js
+++ scripts.js
@@ -2,19 +2,30 @@ const navUl = document.getElementById("nav-ul");
 const button = document.getElementById("nav-hamburger");
 const date = document.getElementById("powered-by-date");
 
+var isOpen = false;
+const maskTTBAnimation = [
+    {transform: 'translateY(50px)'}
+];
+const maskTTBOptions = {
+    duration: 500,
+    easing: 'ease-in-out',
+    fill: 'forwards'
+}
+
 const toggleNav = () => {
-    navUl.classList.toggle("toggle-nav");
-}
-
-const closeNav = () => {
     if(isOpen) {
-        navUl.classList.remove("toggle-nav");
+        navUl.getAnimations().forEach(animation => {
+            animation.reverse();
+        });
+        isOpen = false;
+        return;
     }
+    navUl.animate(maskTTBAnimation, maskTTBOptions);
+    isOpen = isOpen ? false : true;
+    console.log(isOpen);
+
 }
 
-document.getElementById("logo-link").focus();
-
-//window.addEventListener('click', closeNav);
 button.addEventListener('click', toggleNav);
 
 date.innerText = new Date().getFullYear();
