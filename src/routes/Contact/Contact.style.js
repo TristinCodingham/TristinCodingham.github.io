3a4a13c95702f72010993833d8567979065d1416
TristinCodingham <tristinc.codingham@gmail.com>
2021-12-21 17:33:13 +0200

    Update: routes/contact/

--- src/routes/Contact/Contact.style.js
+++ src/routes/Contact/Contact.style.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
 export default styled.main`
-
+text-align: center;
+form {
+    div {
+        display: grid;
+        background-color: #fff;
+        margin: 0 auto;
+    }
+}
 `;
